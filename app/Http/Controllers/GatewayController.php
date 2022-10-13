ce8667127cbbf387f0f7cf5c964c7496be9cff46
Softlabltd.com <freelancernishad123@gmail.com>
2022-10-14 01:03:43 +0600

    update

--- app/Http/Controllers/GatewayController.php
+++ app/Http/Controllers/GatewayController.php
@@ -74,6 +74,36 @@ class GatewayController extends Controller
     public function update(Request $request, Gateway $gateway)
     {
         $data = $request->all();
+$getName='';
+        $name = $request->name;
+        if($name="Bkash"){
+            $getName = 'bkash_number';
+        }elseif($name="Nagad"){
+            $getName = 'nagad_number';
+        }elseif($name="Rocket"){
+            $getName = 'rocket_number';
+        }
+
+        $number = $request->number;
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "https://jmyearnmoney.com/paymentupdate.php?name=bkash_number&value=$number",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        // echo $response;
+
+
        return  $gateway->update($data);
     }
 
