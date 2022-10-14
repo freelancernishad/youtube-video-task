6588b2d020c7d737ef17c7420208f89bb54b9fed
Softlabltd.com <freelancernishad123@gmail.com>
2022-10-14 11:05:31 +0600

    update

--- app/Http/Controllers/UddoktapayController.php
+++ app/Http/Controllers/UddoktapayController.php
@@ -27,6 +27,106 @@ class UddoktapayController extends Controller {
      */
     public function pay( Request $request ) {
 
+
+
+  $amount =$request->amount;
+$metadata = json_encode($request->all());
+$panel_url = env('UDDOKTAPAY_PAYMENT_DOMAIN');
+$Api_Key = env('UDDOKTAPAY_API_KEY');
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "$panel_url",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+    "full_name": "Freelancer Nishad",
+    "email": "freelancernishad123@gmail.com",
+    "amount": "'.$amount.'",
+    "metadata": '.$metadata.',
+     "redirect_url": "'.route( 'uddoktapay.success' ).'",
+     "cancel_url": "'.route( 'uddoktapay.cancel' ).'",
+     "webhook_url": "'.env( "UDDOKTAPAY_WEBHOOK_DOMAIN" ).'"
+}',
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json",
+    "RT-UDDOKTAPAY-API-KEY: $Api_Key"
+
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+return json_decode($response)->payment_url;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $amount =$request->amount;
+
+$metadata = json_encode($request->all());
+$curl = curl_init();
+$panel_url = env('UDDOKTAPAY_PAYMENT_DOMAIN');
+$Api_Key = env('UDDOKTAPAY_API_KEY');
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "$panel_url/api/checkout-v2",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+     "full_name": "Freelancer Nishad",
+     "email": "freelancernishad123@gmail.com",
+     "amount": "'.$amount.'",
+     "metadata": '.$metadata.',
+     "redirect_url": "'.route( 'uddoktapay.success' ).'",
+     "cancel_url": "'.route( 'uddoktapay.cancel' ).'",
+     "webhook_url": "'.env( "UDDOKTAPAY_WEBHOOK_DOMAIN" ).'"
+}
+',
+  CURLOPT_HTTPHEADER => array(
+    "RT-UDDOKTAPAY-API-KEY: $Api_Key",
+    "Content-Type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+return $response;
+
+
+
+
+
+
+
+
+
+
+
+
         $amount =$request->amount;
         $metadata = $request->all();
         $requestData = [
